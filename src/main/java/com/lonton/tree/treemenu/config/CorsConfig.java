1b126975cb03d5812381a55621c5cbcc384517e8
Sally1005 <1564358705@qq.com>
2022-11-09 10:13:08 +0800

    添加配置类，统一处理跨域问题

--- src/main/java/com/lonton/tree/treemenu/config/CorsConfig.java
+++ src/main/java/com/lonton/tree/treemenu/config/CorsConfig.java
@@ -18,9 +18,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                //是否发送Cookie
                 .allowCredentials(true)
-                //放行哪些原始域
                 .allowedOrigins("*")
                 .allowedMethods("*")
                 .allowedHeaders("*")
