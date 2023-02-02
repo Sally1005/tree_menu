9f0c14666d27f38a347c650c24fd627ff60965a1
Sally1005 <1564358705@qq.com>
2023-02-02 18:07:40 +0800

    数据层

---
+++ src/main/java/com/lonton/tree/treemenu/mapper/TreeMenuRoleMapper.java
@@ -0,0 +1,15 @@
+package com.lonton.tree.treemenu.mapper;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * <p>
+ * 菜单角色 Mapper 接口
+ * </p>
+ *
+ * @author 张利红
+ */
+@Repository
+public interface TreeMenuRoleMapper {
+
+}
---
+++ src/main/java/com/lonton/tree/treemenu/mapper/TreeRoleMapper.java
@@ -0,0 +1,15 @@
+package com.lonton.tree.treemenu.mapper;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * <p>
+ * 角色管理 Mapper 接口
+ * </p>
+ *
+ * @author 张利红
+ */
+@Repository
+public interface TreeRoleMapper {
+
+}
