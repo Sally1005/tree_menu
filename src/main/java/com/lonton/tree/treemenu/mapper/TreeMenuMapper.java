a03d87c0548849e572b78873c7ada0ccfc13907a
Sally <1564358705@qq.com>
2023-05-30 19:11:55 +0800

    完善树形菜单 Mapper类

--- src/main/java/com/lonton/tree/treemenu/mapper/TreeMenuMapper.java
+++ src/main/java/com/lonton/tree/treemenu/mapper/TreeMenuMapper.java
@@ -1,7 +1,6 @@
 package com.lonton.tree.treemenu.mapper;
 
-import com.lonton.tree.treemenu.pojo.TreeMenu;
-import org.apache.ibatis.annotations.Mapper;
+import com.lonton.tree.treemenu.pojo.entity.TreeMenu;
 
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
@@ -9,42 +8,49 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 /**
- *(TreeMenu)表数据库访问层
+ * 树形菜单 Mapper类
  *
  * @author 张利红
  */
-@Mapper
 @Repository
 public interface TreeMenuMapper {
     /**
-     * 获取所有数据 <br/>
-     * @return 整棵树菜单
+     * 获取所有数据
+     *
+     * @return 树形菜单
      */
     List<TreeMenu> getAllTreeMenu();
 
     /**
-     * 获取根节点菜单 <br/>
+     * 获取根节点菜单
+     *
      * @return 根节点菜单
      */
     List<TreeMenu> getRootMenus();
 
     /**
-     * 根据父节点查询子节点数据 <br/>
-     * @param menuId <br/>
+     * 根据父节点查询子节点数据
+     *
+     * @param menuId 节点id
      * @return 子节点
      */
     List<TreeMenu> getChildren(@Param("menuId") Long menuId);
 
     /**
-     * 判断是否是叶子节点 <br/>
-     * @param menuId <br/>
-     * @return 是否
+     * 根据菜单Id查询
+     *
+     * @param menuId 菜单id
+     * @return 菜单树
      */
-    Boolean isLeaf(Long menuId);
+    List<TreeMenu> getMenuById(@Param("menuId")Long menuId);
 
-    List<TreeMenu> buildTree();
-
-    List<TreeMenu> searchItems(@Param("name")String name);
+    /**
+     * 根据菜单名称查询
+     *
+     * @param menuName 菜单名
+     * @return 菜单树
+     */
+    List<TreeMenu> searchItems(@Param("menuName") String menuName);
 
 }
 
