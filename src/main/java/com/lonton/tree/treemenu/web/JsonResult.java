3f6d6b98cae4c52a0935c5324baf39b4cba2ff21
Sally <1564358705@qq.com>
2023-06-29 18:25:37 +0800

    统一的响应结果类型

---
+++ src/main/java/com/lonton/tree/treemenu/web/JsonResult.java
@@ -0,0 +1,57 @@
+package com.lonton.tree.treemenu.web;
+
+import com.lonton.tree.treemenu.common.exception.GlobalException;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 统一的响应结果类型
+ *
+ * @author 张利红
+ */
+@Data
+public class JsonResult<T> implements Serializable {
+
+    /**
+     * 业务状态码
+     */
+    private Integer state;
+    /**
+     * 消息
+     */
+    private String message;
+    /**
+     * 数据
+     */
+    private T data;
+
+    public static JsonResult ok() {
+        return ok(null);
+    }
+
+    public static <T> JsonResult<T> ok(T data) {
+        JsonResult jsonResult = new JsonResult();
+        jsonResult.setState(ServiceCode.OK);
+        jsonResult.setData(data);
+        return jsonResult;
+    }
+
+    public static JsonResult<Void> fail(GlobalException e) {
+        return fail(e.getCode(), e.getMessage());
+    }
+    public static JsonResult<String> error(Integer state, String message) {
+        JsonResult jsonResult = new JsonResult();
+        jsonResult.setState(state);
+        jsonResult.setMessage(message);
+        return jsonResult;
+    }
+
+    public static JsonResult<Void> fail(Integer state, String message) {
+        JsonResult jsonResult = new JsonResult();
+        jsonResult.setState(state);
+        jsonResult.setMessage(message);
+        return jsonResult;
+    }
+
+}
