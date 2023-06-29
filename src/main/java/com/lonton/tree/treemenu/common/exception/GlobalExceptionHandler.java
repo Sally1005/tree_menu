3acc5cb10490af83fb665b8795dcda952c65ffa0
Sally <1564358705@qq.com>
2023-06-29 18:20:53 +0800

    全局异常处理类

--- src/main/java/com/lonton/tree/treemenu/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/lonton/tree/treemenu/common/exception/GlobalExceptionHandler.java
@@ -1,50 +1,79 @@
 package com.lonton.tree.treemenu.common.exception;
 
-import com.lonton.tree.treemenu.common.util.Result;
+import com.lonton.tree.treemenu.web.JsonResult;
+import com.lonton.tree.treemenu.web.ServiceCode;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.validation.BindException;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.List;
+
 /**
  * 全局异常处理类
- * <p/>
+ *
  * @author 张利红
  */
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-
-    /**
-     * 处理 Exception 异常<br/>
-     * @param e 异常<br/>
-     * @return 处理结果
-     */
-    @ExceptionHandler(Exception.class)
-    public Result handlerException(Exception e) {
-        log.error(e.getMessage(), e);
-        return Result.error().message("系统异常");
+    public GlobalExceptionHandler() {
+        log.debug("创建统一处理异常的类：GlobalExceptionHandler");
     }
 
-    /**
-     * 处理空指针异常<br/>
-     * @param e 异常<br/>
-     * @return 处理结果
-     */
-    @ExceptionHandler(NullPointerException.class)
-    public Result handlerNullPointerException(NullPointerException e) {
-        log.error(e.getMessage(), e);
-        return Result.error().message("空指针异常");
+    @ExceptionHandler
+    public JsonResult<Void> handleServiceException(GlobalException e) {
+        log.debug("处理ServiceException：{}", e.getMessage());
+        return JsonResult.fail(e);
     }
 
-    /**
-     * 处理自定义异常<br/>
-     * @param e 异常<br/>
-     * @return 处理结果
-     */
-    @ExceptionHandler(GlobalException.class)
-    public Result handlerGlobalException(GlobalException e) {
-        log.error(e.getMessage(), e);
-        return Result.error().message(e.getMessage()).code(e.getCode());
+    @ExceptionHandler
+    public JsonResult<Void> handleBindException(BindException e) {
+        log.debug("处理BindException：{}", e.getMessage());
+
+        StringBuilder stringBuilder = new StringBuilder();
+        List<FieldError> fieldErrors = e.getFieldErrors();
+        for (FieldError fieldError : fieldErrors) {
+            String message = fieldError.getDefaultMessage();
+            stringBuilder.append(message);
+        }
+
+        return JsonResult.fail(ServiceCode.ERR_BAD_REQUEST, stringBuilder.toString());
     }
+
+    @ExceptionHandler
+    public JsonResult<Void> handleBadCredentialsException(BadCredentialsException e) {
+        String message = "登录失败，用户名或密码错误！";
+        log.debug("处理BadCredentialsException：{}", message);
+        return JsonResult.fail(ServiceCode.ERR_UNAUTHORIZED, message);
+    }
+
+    @ExceptionHandler
+    public JsonResult<Void> handleAccessDeniedException(AccessDeniedException e) {
+        String message = "访问失败，当前登录的账号无此权限！";
+        log.debug("处理AccessDeniedException：{}", message);
+        return JsonResult.fail(ServiceCode.ERR_FORBIDDEN, message);
+    }
+
+    @ExceptionHandler
+    public JsonResult<Void> handleDisabledException(DisabledException e) {
+        String message = "登录失败，此账号已禁用！";
+        log.debug("处理DisabledException：{}", message);
+        return JsonResult.fail(ServiceCode.ERR_UNAUTHORIZED_DISABLED, message);
+    }
+
+
+    @ExceptionHandler
+    public JsonResult<Void> handleThrowable(Throwable e) {
+        log.debug("处理Throwable");
+        e.printStackTrace();
+        String message = "程序运行过程中出现意外错误，请联系系统管理员！";
+        return JsonResult.fail(ServiceCode.ERR_INTERNAL_SERVER_ERROR, message);
+    }
+
 }
