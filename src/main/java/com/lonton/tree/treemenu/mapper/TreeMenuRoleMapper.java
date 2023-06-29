027e5eb1444eb2a111f051d940c75d96ef6304f4
Sally <1564358705@qq.com>
2023-06-29 18:22:59 +0800

    处理菜单角色的Mapper接口

--- src/main/java/com/lonton/tree/treemenu/mapper/TreeMenuRoleMapper.java
+++ src/main/java/com/lonton/tree/treemenu/mapper/TreeMenuRoleMapper.java
@@ -1,15 +1,31 @@
 package com.lonton.tree.treemenu.mapper;
 
+import com.lonton.tree.treemenu.pojo.entity.TreeMenu;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
- * <p>
- * 菜单角色 Mapper 接口
- * </p>
+ * 处理菜单角色的Mapper接口
  *
  * @author 张利红
  */
 @Repository
 public interface TreeMenuRoleMapper {
+//    /**
+//     * 根据角色菜单
+//     *
+//     * @param role 角色
+//     * @return 菜单数据
+//     */
+//    List<TreeMenu> getMenuByRole(@PathVariable("role") String role);
+
+    List<TreeMenu> getMenuDataForRoot();
+
+    List<TreeMenu> getMenuDataForMonth();
+
+    List<TreeMenu> getMenuDataForQuarter();
+
+    List<TreeMenu> getMenuDataForYear();
 
 }
