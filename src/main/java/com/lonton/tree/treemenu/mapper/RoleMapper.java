b0eb8e24388b72d579dce805f066806de60ca675
Sally <1564358705@qq.com>
2023-07-10 16:27:43 +0800

    添加方法

--- src/main/java/com/lonton/tree/treemenu/mapper/RoleMapper.java
+++ src/main/java/com/lonton/tree/treemenu/mapper/RoleMapper.java
@@ -1,6 +1,8 @@
 package com.lonton.tree.treemenu.mapper;
 
+import com.lonton.tree.treemenu.pojo.entity.Role;
 import com.lonton.tree.treemenu.pojo.vo.RoleListItemVO;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -19,4 +21,11 @@ public interface RoleMapper {
      */
     List<RoleListItemVO> list();
 
+    /**
+     * 查询当前用户下的角色列表
+     *
+     * @param userId 用户id
+     * @return 角色列表
+     */
+    List<Role> findListByUserId(@Param("userId") Long userId);
 }
