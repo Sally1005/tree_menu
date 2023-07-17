d1be0b0e82948b2cdd3442f5cac238032e8562b6
Sally <1564358705@qq.com>
2023-07-17 12:04:07 +0800

    添加处理 menuTreePath 的逻辑

--- src/main/java/com/lonton/tree/treemenu/service/impl/MenuServiceImpl.java
+++ src/main/java/com/lonton/tree/treemenu/service/impl/MenuServiceImpl.java
@@ -1,7 +1,9 @@
 package com.lonton.tree.treemenu.service.impl;
 
+import com.alibaba.druid.util.StringUtils;
 import com.lonton.tree.treemenu.mapper.TreeMenuMapper;
 import com.lonton.tree.treemenu.pojo.entity.TreeMenu;
+import com.lonton.tree.treemenu.pojo.vo.MenuSearchVO;
 import com.lonton.tree.treemenu.service.MenuService;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +12,8 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * 菜单实现类
@@ -37,16 +41,49 @@ public class MenuServiceImpl implements MenuService {
         // 获取子菜单列表
         List<TreeMenu> childrenList = menuMapper.getChildren(menuId);
         // 加载菜单
-        loadMenus(menuById,childrenList);
+        loadMenus(menuById, childrenList);
         List<TreeMenu> result = new ArrayList<>();
         result.add(menuById);
         return result;
     }
 
+    @Override
+    public List<TreeMenu> searchMenuByName(Long menuId, String menuName) {
+        // 创建了一个MenuSearchVO对象
+        MenuSearchVO menuSearchVO = new MenuSearchVO();
+        // 设置了菜单ID和菜单名称
+        menuSearchVO.setMenuName(menuName);
+        menuSearchVO.setMenuId(menuId);
+        List<TreeMenu> searchMenus = menuMapper.searchMenuByIdAndName(menuSearchVO);
+        // 存储所有菜单ID
+        Set<Long> allMenuIds = new HashSet<>();
+        // 遍历获取所有符合条件的菜单路径,并以"-"分割菜单树路径字符串
+        for (TreeMenu searchMenu : searchMenus) {
+            String[] split = searchMenu.getMenuTreePath().split("-");
+            // 逐个判断分割后的子字符串是否为数字，如果是数字，则将其转换为Long类型,并添加到一个集合中(allMenuIds)
+            for (String s : split) {
+                if (StringUtils.isNumber(s)) {
+                    allMenuIds.add(Long.valueOf(s));
+                }
+            }
+        }
+        // 如果没有搜索到符合条件的菜单，此时返回一个空的ArrayList
+        if (allMenuIds.size() == 0) {
+            return new ArrayList<>();
+        }
+        // 如果allMenuIds集合不为空，则将其转换为一个menuIdList列表
+        List<Long> menuIdList = new ArrayList<>(allMenuIds);
+        // 调用menuMapper的findListByMenuIds方法，根据菜单ID列表获取到对应的菜单列表
+        List<TreeMenu> menuList = menuMapper.findListByMenuIds(menuIdList);
+        // 调用buildTree方法，将菜单列表构建成一个树形结构
+        return buildTree(menuList);
+
+    }
+
     /**
      * 加载菜单
      *
-     * @param menuById 菜单
+     * @param menuById     菜单
      * @param childrenList 子菜单列表
      */
     private void loadMenus(TreeMenu menuById, List<TreeMenu> childrenList) {
